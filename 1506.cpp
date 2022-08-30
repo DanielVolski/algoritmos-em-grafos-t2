49d8520004ec1d38072e6d107f98bebe1fd37a84
Daniel Volski <danfvdaum3@hotmail.com>
2022-08-30 19:32:32 -0300

    Dijkstra algorithm implemented

--- 1506.cpp
+++ 1506.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <limits>
+
+int dijkstra(std::vector<std::tuple<int, int, int>> graph, int start, int end, int qt_vertexes);
 
 int main()
 {
@@ -25,7 +28,7 @@ int main()
         for (int i = 0; i < m; i++)
         {
             std::cin >> u >> v >> w;
-            graph.push_back(std::make_tuple(u, v, w));
+            graph.push_back(std::make_tuple(u - 1, v - 1, w));
         }
 
         std::cin >> k >> p;
@@ -36,8 +39,45 @@ int main()
             objects_position.push_back(choosen_vertex);
         }
 
+        std::cout << dijkstra(graph, 0, 2, n) << std::endl;
+
         t--;
     }
-
     return 0;
+}
+
+int dijkstra(std::vector<std::tuple<int, int, int>> graph, int start, int end, int qt_vertexes)
+{
+    int distances[qt_vertexes];
+    std::vector<std::tuple<int, int, int>> adj;
+
+    for (int i = 0; i < qt_vertexes; i++)
+        distances[i] = std::numeric_limits<int>::max();
+
+    distances[start] = 0;
+
+    for (int i = 0; i < qt_vertexes; i++)
+    {
+        adj.clear();
+        // This is going to get all edges that are adjascent to i
+        for (int j = 0; j < graph.size(); j++)
+        {
+            if (std::get<0>(graph[j]) == i || std::get<1>(graph[j]) == i)
+            {
+                adj.push_back(graph[j]);
+                // This is just to "delete" the edges from the graph
+                graph[j] = std::make_tuple(0, 0, 0);
+            }
+        }
+        
+        // This is going to relax all the adjascents to i
+        for (int j = 0; j < adj.size(); j++)
+        {
+            if (std::get<0>(adj[j]) != i && distances[std::get<0>(adj[j])] > std::get<2>(adj[j]))
+                distances[std::get<0>(adj[j])] = distances[i] + std::get<2>(adj[j]);
+            else if (std::get<1>(adj[j]) != i && distances[std::get<1>(adj[j])] > std::get<2>(adj[j]))
+                distances[std::get<1>(adj[j])] = distances[i] + std::get<2>(adj[j]);
+        }
+    }
+    return distances[end];
 }
